5153fccb018708fc64e85a764f28e8b072c04449
arif-ahmed <khandaker.arif.ahmed@gmail.com>
2025-06-27 21:24:58 +0600

    updated

---
+++ backend/src/AccountingLedger.Application/Queries/GetAllAccountsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿
+using MediatR;
+
+namespace AccountingLedger.Application.Queries;
+
+public class GetAllAccountsQueryHandler : IRequestHandler<GetAllAccountsQuery, List<AccountDto>>
+{
+    // private readonly IAccountRepository _accountRepository;
+    public GetAllAccountsQueryHandler()
+    {
+        //_accountRepository = accountRepository;
+    }
+    public async Task<List<AccountDto>> Handle(GetAllAccountsQuery request, CancellationToken cancellationToken)
+    {
+        // var accounts = await _accountRepository.GetAllAsync(cancellationToken);
+        //return accounts.Select(a => new AccountDto
+        //{
+        //    Id = a.Id,
+        //    Name = a.Name,
+        //    Type = a.Type
+        //}).ToList();
+        return new();
+    }
+}
